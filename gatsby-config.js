e32daef571b9ce1d9ccd08cd9c1cebabd9e518d3
lemoner-rajesh <rajesh@lemonadverts.com>
2022-01-07 11:17:57 +0400

    test on pantheornsite

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,22 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://dev-drupal-lemon.pantheonsite.io/",
     title: "app1",
   },
   plugins: [
     {
       resolve: `gatsby-source-drupal`,
       options: {
+        // apiKey: process.env.API_KEY,
         preview:true,
         baseUrl: `https://dev-drupal-lemon.pantheonsite.io/`,
-        // secret: process.env.PREVIEW_SECRET,
-        // fastBuilds: true,
+        fastBuilds: true,
+        secret: `123456`
       
       },
       
